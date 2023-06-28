aaecddbebdb390a2d1011b670648710c27d854c8
iritmaximus <aaro.koivukangas@gmail.com>
2023-06-28 19:56:27 +0300

    added eslint for reasons

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// .eslintrc.js example
+module.exports = {
+  "env": {
+      "browser": true,
+      "es2021": true
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
+  "rules": {
+    "semi": [1, "always"],
+    "quotes": [1, "double"],
+    "react/prop-types": 1,
+  }
+};
