f82acdf177312b792467c72cf3de5f1618af6fca
iritmaximus <aaro.koivukangas@gmail.com>
2023-06-29 12:21:40 +0300

    moved login and blogs to own components

--- src/App.js
+++ src/App.js
@@ -11,6 +11,45 @@ const App = () => {
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
 
+  const loginForm = () => {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <input
+              type="text"
+              value={username}
+              name="username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              value={password}
+              name="password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    );
+  };
+
+  const blogForm = () => {
+    return (
+      <div>
+        {user.name} logged in
+        <button type="button" onClick={handleLogout}>logout</button>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    );
+  };
+
   useEffect(() => {
     const fetchBlogs = async () => {
       const blogs = await blogService.getAll();
@@ -49,43 +88,13 @@ const App = () => {
     setUser(null);
   };
 
-  if (!user) {
-    return (
-      <div>
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            <input
-              type="text"
-              value={username}
-              name="username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              value={password}
-              name="password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h2>blogs</h2>
-        <p>{user.name} logged in</p>
-        <button type="button" onClick={handleLogout}>logout</button>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Blogs</h1>
+      {!user && loginForm()}
+      {user && blogForm()}
+    </div>
+  );
 };
 
 export default App;
