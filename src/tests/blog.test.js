a88c0c389383e71cb3165700c2a3915efd84456c
iritmaximus <aaro.koivukangas@gmail.com>
2023-07-04 10:00:01 +0300

    todo for tests

---
+++ src/tests/blog.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+
+import { Blog } from "../components/Blog";
+
+
+describe("Blogs", () => {
+  it.todo("renders only title + author by default");
+  it.todo("doesn't render url + likes by default");
+  it.todo("renders likes + url after button is pressed");
+  it.todo("if like is pressed twice, same handlerfunc is called twice");
+  it.todo("form calls handlerfunc with correct data");
+});
