f3e7815e167cb2f71749e7e35fc4f8fb04087618
iritmaximus <aaro.koivukangas@gmai.com>
2023-07-07 22:53:22 +0300

    bugfixing matters

--- src/components/blog.test.js
+++ src/components/blog.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
 
-import { Blog } from "../components/Blog";
+import { Blog } from "./Blog";
 
 
 describe("Blogs", () => {
